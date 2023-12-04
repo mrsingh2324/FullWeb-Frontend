0d89efb3090708aee359aa917b70842766a9eca9
Satyam Singh <87685197+mrsingh2324@users.noreply.github.com>
2023-12-04 17:28:59 +0530

    Update UserInbox.jsx

--- src/pages/UserInbox.jsx
+++ src/pages/UserInbox.jsx
@@ -9,7 +9,7 @@ import { useNavigate } from "react-router-dom";
 import { AiOutlineArrowRight, AiOutlineSend } from "react-icons/ai";
 import { TfiGallery } from "react-icons/tfi";
 import styles from "../styles/styles";
-const ENDPOINT = "https://full-web-socket.vercel.app/";
+const ENDPOINT = "https://fullweb-socket.onrender.com/";
 const socketId = socketIO(ENDPOINT, { transports: ["websocket"] });
 
 const UserInbox = () => {
