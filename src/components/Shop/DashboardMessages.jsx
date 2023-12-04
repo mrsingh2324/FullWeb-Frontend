27cda1a1a4fec8cc70e3b89c8e420cc72cec3e0c
Satyam Singh <87685197+mrsingh2324@users.noreply.github.com>
2023-12-04 17:27:45 +0530

    Update DashboardMessages.jsx

--- src/components/Shop/DashboardMessages.jsx
+++ src/components/Shop/DashboardMessages.jsx
@@ -9,7 +9,7 @@ import styles from "../../styles/styles";
 import { TfiGallery } from "react-icons/tfi";
 import socketIO from "socket.io-client";
 import { format } from "timeago.js";
-const ENDPOINT = "https://full-web-socket.vercel.app/";
+const ENDPOINT = "https://fullweb-socket.onrender.com/";
 const socketId = socketIO(ENDPOINT, { transports: ["websocket"] });
 
 const DashboardMessages = () => {
